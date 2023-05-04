ff6da4c62dbcbc62e6976c091d816343910b7bec
evanofslack <evan.slack@outlook.com>
2023-05-04 02:03:44 -0400

    update nvim dashboard

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -3,8 +3,7 @@ if not status_ok then
 	return
 end
 
-local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
+local neovim = {
 	[[                               __                ]],
 	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
 	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
@@ -13,25 +12,65 @@ dashboard.section.header.val = {
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 
-dashboard.section.buttons.val = {
-	dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-	dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+local slacknet = {
+	[[                                                                    ]],
+	[[                                                                    ]],
+	[[                                                                    ]],
+	[[       ___                       __                         __      ]],
+	[[      /\_ \                     /\ \                       /\ \__   ]],
+	[[  ____\//\ \       __       ___ \ \ \/'\      ___       __ \ \ ,_\  ]],
+	[[ /',__\ \ \ \    /'__`\    /'___\\ \ , <    /' _ `\   /'__`\\ \ \/  ]],
+	[[/\__, `\ \_\ \_ /\ \L\.\_ /\ \__/ \ \ \\`\  /\ \/\ \ /\  __/ \ \ \_ ]],
+	[[\/\____/ /\____\\ \__/.\_\\ \____\ \ \_\ \_\\ \_\ \_\\ \____\ \ \__\]],
+	[[ \/___/  \/____/ \/__/\/_/ \/____/  \/_/\/_/ \/_/\/_/ \/____/  \/__/]],
+	[[                                                                    ]],
+	[[                                                                    ]],
 }
 
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = slacknet
+
+dashboard.section.buttons.val = {
+	dashboard.button("t", " " .. " new file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", " " .. " find file", ":Telescope find_files <CR>"),
+	dashboard.button("r", " " .. " recent files", ":Telescope oldfiles <CR>"),
+	-- dashboard.button("g", " " .. " find text", ":Telescope live_grep <CR>"),
+	dashboard.button("n", "פּ " .. " nvim-tree", ":NvimTreeToggle<CR>"),
+	dashboard.button(
+		"l",
+		" " .. " git",
+		":lua require('toggleterm.terminal').Terminal:new({cmd = 'lazygit', direction = 'float'}):toggle()<cr>"
+	),
+	dashboard.button("u", "ﮮ " .. " update plugins", ":PackerUpdate <CR>"),
+	dashboard.button("s", " " .. " sync plugins", ":PackerUpdate <CR>"),
+	dashboard.button("c", " " .. " config", ":e $MYVIMRC <CR>"),
+	dashboard.button("q", " " .. " quit", ":qa<CR>"),
+}
+
+-- local function footer()
+-- 	-- local plugins = #vim.tbl_keys(packer_plugins)
+-- 	-- local plugin_info = " " .. plugins
+-- 	local datetime = os.date(" %m-%d-%Y   %H:%M")
+-- 	local version = vim.version()
+-- 	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+--
+-- 	-- return plugin_info .. datetime .. nvim_version_info
+-- 	return datetime .. nvim_version_info
+-- end
+
 local function footer()
-	-- local plugins = #vim.tbl_keys(packer_plugins)
-	-- local plugin_info = " " .. plugins
-	local datetime = os.date(" %m-%d-%Y   %H:%M")
+	local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
 	local version = vim.version()
 	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-	-- return plugin_info .. datetime .. nvim_version_info
-	return datetime .. nvim_version_info
+	return datetime .. "   " .. " plugins" .. nvim_version_info
 end
+
 dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "GruvboxYellow"
+dashboard.section.header.opts.hl = "GruvboxBlue"
+-- dashboard.section.buttons.opts.hl = "GruvboxGreen"
 
 alpha.setup(dashboard.opts)

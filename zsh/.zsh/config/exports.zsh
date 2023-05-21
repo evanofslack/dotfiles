f87c7fe8c62a73ab81b09eaf51a69f4b1a03454a
evan slack <evan.slack@outlook.com>
2023-05-21 15:05:28 -0400

    zsh updates

--- zsh/.zsh/config/exports.zsh
+++ zsh/.zsh/config/exports.zsh
@@ -1,8 +1,8 @@
 export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH" # mysql
-export PATH="/Users/eslack/.cargo/bin:$PATH" # cargo
 export PATH=$PATH:$(go env GOPATH)/bin # golang
 export PATH=$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH # find
 export PATH="${PATH}:${HOME}/.krew/bin" # krew
+export PATH="$HOME/.cargo/bin:$PATH" # cargo
 
 # pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -12,5 +12,5 @@ eval "$(pyenv init -)"
 # editor
 export EDITOR=nvim
 
-
+# gvm
 [[ -s "/Users/eslack/.gvm/scripts/gvm" ]] && source "/Users/eslack/.gvm/scripts/gvm"

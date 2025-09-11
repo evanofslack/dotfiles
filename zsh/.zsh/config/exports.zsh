55f73c0dfcedae17a9406706a5022c80f55450d5
evan slack <evan.slack@outlook.com>
2025-09-11 10:17:36 -0400

    more zsh exports

--- zsh/.zsh/config/exports.zsh
+++ zsh/.zsh/config/exports.zsh
@@ -4,6 +4,7 @@ export PATH=$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH # find
 export PATH="${PATH}:${HOME}/.krew/bin" # krew
 export PATH="$HOME/.cargo/bin:$PATH" # cargo
 export PATH="$HOME/.local/share/bob/nvim-bin:$PATH" # nvim manager
+export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH" # mysql older version
 
 # pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -22,3 +23,9 @@ source <(switcher init zsh)
 
 # gvm
 [[ -s "/Users/eslack/.gvm/scripts/gvm" ]] && source "/Users/eslack/.gvm/scripts/gvm"
+
+# Docker/Colima testcontainers
+export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
+export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE="/var/run/docker.sock"
+export UV_ENV_FILE=.env
+export DAGSTER_HOME="$(pwd)/dagster_home"

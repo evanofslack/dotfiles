777096a8eb143ff95407264f307e4f4a5614212c
Evan Slack <evan.slack@outlook.com>
2025-09-14 18:46:33 -0400

    more exports (new mac)

--- zsh/.zsh/config/exports.zsh
+++ zsh/.zsh/config/exports.zsh
@@ -4,6 +4,7 @@ export PATH=$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH # find
 export PATH="${PATH}:${HOME}/.krew/bin" # krew
 export PATH="$HOME/.cargo/bin:$PATH" # cargo
 export PATH="$HOME/.local/share/bob/nvim-bin:$PATH" # nvim manager
+export NVM_DIR="$HOME/.nvm" # node version manager
 
 # pyenv
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -22,3 +23,11 @@ source <(switcher init zsh)
 
 # gvm
 [[ -s "/Users/eslack/.gvm/scripts/gvm" ]] && source "/Users/eslack/.gvm/scripts/gvm"
+
+# substring search
+source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+
+# node version manager
+[ -s "$(brew --prefix nvm)/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"
+[ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
